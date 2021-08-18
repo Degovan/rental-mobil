f213dee1d375c251202d384fae6330f36a88152c
Mohammad Sahrullah <sahrul-dev@github.com>
2021-08-18 15:03:46 +0700

    help bikinin view layout

---
+++ app/Views/layout/app.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords"
+        content="wrappixel, admin dashboard, html css dashboard, web dashboard, bootstrap 5 admin, bootstrap 5, css3 dashboard, bootstrap 5 dashboard, Monsterlite admin bootstrap 5 dashboard, frontend, responsive bootstrap 5 admin template, Monster admin lite design, Monster admin lite dashboard bootstrap 5 dashboard template">
+    <meta name="description"
+        content="Monster Lite is powerful and clean admin dashboard template, inpired from Bootstrap Framework">
+    <meta name="robots" content="noindex,nofollow">
+    <title>Monster Lite Template by WrapPixel</title>
+    <link rel="canonical" href="https://www.wrappixel.com/templates/monster-admin-lite/" />
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= echo base_url();?>assets/images/favicon.png">
+    <!-- Custom CSS -->
+    <link href="<?= echo base_url();?>assets/plugins/chartist/dist/chartist.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="<?= echo base_url();?>css/style.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    
+    <?= $this->extend('layout/header') ?>
+    <?= $this->extend('layout/navbar') ?>
+    <?= $this->renderSection('content') ?>
+   <?= $this->extend('layout/footer') ?>
+    <script src="../assets/plugins/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="../assets/plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/app-style-switcher.js"></script>
+    <!--Wave Effects -->
+    <script src="js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="js/custom.js"></script>
+    <!--This page JavaScript -->
+    <!--flot chart-->
+    <script src="public/assets/plugins/flot/jquery.flot.js"></script>
+    <script src="public/assets/plugins/flot.tooltip/js/jquery.flot.tooltip.min.js"></script>
+    <script src="public/js/pages/dashboards/dashboard1.js"></script>
+</body>
+
+</html>

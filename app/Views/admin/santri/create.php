c4026920140b201d9e179b3c0f3150727444d8af
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2021-08-19 23:16:48 +0700

    chore(santri): import excel form

--- app/Views/admin/santri/create.php
+++ app/Views/admin/santri/create.php
@@ -18,5 +18,27 @@
             </div>
         </div>
     </div>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Import Excel</h5>
+
+                <form action="<?= route_to('santri.excel') ?>" method="post" enctype="multipart/form-data">
+                    <?= csrf_field() ?>
+                    <div class="form-floating mb-3">
+                        <input type="file" name="excel" id="excel" class="form-control <?= ($errors['excel'] ?? null) ? 'is-invalid' : '' ?>" accept=".xlsx,.xsl">
+                        <div class="invalid-feedback">
+                            <?= $errors['excel'] ?? '' ?>
+                        </div>
+                    </div>
+                    <div class="mb-3 text-center">
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fa fa-download"></i> Import
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 <?= $this->endSection() ?>
